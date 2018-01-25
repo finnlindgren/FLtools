6adefcc0d93577e84aa200ee016aa762b95c4406
Finn Lindgren <finn.lindgren@gmail.com>
2018-01-25 23:55:41 +0000

    Further arra/scalar corrections

--- R/optimisation.R
+++ R/optimisation.R
@@ -534,10 +534,10 @@ optimisation.app <- function() {
               ) %*% t(EH$vectors)
               curve(as.vector(state$fn$f(curr_x)) +
                       (x - curr_x_v) * as.vector(state$fn$g(curr_x, state$fn$f)) +
-                        (x - curr_x_v) ^ 2 / 2 * H, add = TRUE, col = 2)
+                        (x - curr_x_v) ^ 2 / 2 * as.vector(H), add = TRUE, col = 2)
               curve(as.vector(state$fn$f(curr_x)) +
                       (x - curr_x_v) * as.vector(state$fn$g(curr_x, state$fn$f)) +
-                        (x - curr_x_v) ^ 2 / 2 * H2, add = TRUE, col = 4)
+                        (x - curr_x_v) ^ 2 / 2 * as.vector(H2), add = TRUE, col = 4)
             } else if (input$method == "bfgs") {
               H <- state$fn$h(curr_x, state$fn$f)
               if (is.null(state$x.state$Hinv)) {
@@ -550,10 +550,12 @@ optimisation.app <- function() {
               } else {
                 H2 <- solve(state$x.state$Hinv)
               }
-              curve(state$fn$f(curr_x) + (x - curr_x_v) * state$fn$g(curr_x, state$fn$f) +
-                (x - curr_x) ^ 2 / 2 * H, add = TRUE, col = 2)
-              curve(state$fn$f(curr_x) + (x - curr_x_v) * state$fn$g(curr_x, state$fn$f) +
-                (x - curr_x) ^ 2 / 2 * H2, add = TRUE, col = 4)
+              curve(state$fn$f(curr_x) +
+                      (x - curr_x_v) * as.vector(state$fn$g(curr_x, state$fn$f)) +
+                        (x - curr_x) ^ 2 / 2 * as.vector(H), add = TRUE, col = 2)
+              curve(state$fn$f(curr_x) +
+                      (x - curr_x_v) * as.vector(state$fn$g(curr_x, state$fn$f)) +
+                        (x - curr_x) ^ 2 / 2 * as.vector(H2), add = TRUE, col = 4)
             }
           } else {
             curr_x <- state$x.state$x
